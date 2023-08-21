f1c09a17285cc3fc4b308155bad593b380c9d55b
dangminhngo <dangminhngo.dev@gmail.com>
2023-08-21 23:55:17 +0700

    feat(api): export trpc and its types to reuse on client

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -1,1 +1,10 @@
-export * from "./src"
+import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server"
+
+import type { AppRouter } from "./src/routers/_app"
+
+export { appRouter, type AppRouter } from "./src/routers/_app"
+export { createTRPCContext } from "./src/trpc"
+
+// @see https://trpc.io/docs/client/vanilla/infer-types#inferring-input--output-types
+export type RouterInputs = inferRouterInputs<AppRouter>
+export type RouterOutputs = inferRouterOutputs<AppRouter>
