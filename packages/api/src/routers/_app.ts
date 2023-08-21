d28d279cec5044ef3dad28d53738256f3637e8b1
dangminhngo <dangminhngo.dev@gmail.com>
2023-08-22 06:24:05 +0700

    fix(api): merge routers

--- packages/api/src/routers/_app.ts
+++ packages/api/src/routers/_app.ts
@@ -1,5 +1,12 @@
 import { router } from "../trpc"
+import { pageRouter } from "./page"
+import { sessionRouter } from "./session"
+import { userRouter } from "./user"
 
-export const appRouter = router({})
+export const appRouter = router({
+  page: pageRouter,
+  session: sessionRouter,
+  user: userRouter,
+})
 
 export type AppRouter = typeof appRouter
