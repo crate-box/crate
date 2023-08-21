bd4a73532ef2a24f1d01d44f5d3a8f948cac58db
dangminhngo <dangminhngo.dev@gmail.com>
2023-08-22 06:23:56 +0700

    feat(api): migrate page router

---
+++ packages/api/src/routers/page.ts
@@ -0,0 +1,132 @@
+import { TRPCError } from "@trpc/server"
+import { z } from "zod"
+
+import type { Prisma } from "@acme/db"
+
+import { defaultPageSelect, singlePageSelect } from "../selects"
+import { protectedProcedure, router } from "../trpc"
+
+export const pageRouter = router({
+  create: protectedProcedure
+    .input(
+      z.object({
+        data: z.object({
+          icon: z.string().optional(),
+          title: z.string().min(1),
+          body: z.string().optional(),
+        }),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.page.create({
+        data: {
+          ...input.data,
+          creatorId: ctx.session.user.id,
+          body: input.data.body ?? "",
+        },
+      })
+    }),
+  all: protectedProcedure
+    .input(
+      z
+        .object({
+          query: z.string(),
+          pinned: z.boolean(),
+          trashed: z.boolean(),
+          filters: z.object({
+            startDate: z.date(),
+            endDate: z.date(),
+          }),
+          sort: z
+            .array(
+              z.union([
+                z.object({ title: z.enum(["asc", "desc"]) }),
+                z.object({ createdAt: z.enum(["asc", "desc"]) }),
+                z.object({ updatedAt: z.enum(["asc", "desc"]) }),
+              ])
+            )
+            .default([
+              { title: "asc" },
+              { createdAt: "desc" },
+              { updatedAt: "desc" },
+            ]),
+        })
+        .partial()
+        .optional()
+    )
+    .query(({ ctx, input }) => {
+      const where: Prisma.PageWhereInput = {
+        creatorId: ctx.session.user.id,
+        trashed: false,
+        OR: [{ space: { trashed: false } }, { space: null }],
+      }
+
+      if (input?.query) {
+        where.title = { contains: input.query, mode: "insensitive" }
+      }
+
+      if (input?.pinned) {
+        where.pinned = input.pinned
+      }
+
+      if (input?.trashed) {
+        where.trashed = input.trashed
+      }
+
+      return ctx.prisma.page.findMany({ where, select: defaultPageSelect })
+    }),
+  byId: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const page = await ctx.prisma.page.findUnique({
+        where: {
+          id: input.id,
+          OR: [
+            { creatorId: ctx.session.user.id },
+            { space: { members: { some: { id: ctx.session.user.id } } } },
+          ],
+        },
+        select: singlePageSelect,
+      })
+
+      if (!page) {
+        throw new TRPCError({ code: "NOT_FOUND" })
+      }
+
+      return page
+    }),
+  update: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        data: z
+          .object({
+            icon: z.string(),
+            title: z.string(),
+            description: z.string(),
+            body: z.string(),
+            pinned: z.boolean(),
+            trashed: z.boolean(),
+          })
+          .partial(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.page.update({
+        where: { id: input.id, creatorId: ctx.session.user.id },
+        data: {
+          ...input.data,
+          editorId: ctx.session.user.id,
+        },
+        select: defaultPageSelect,
+      })
+    }),
+  delete: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.page.delete({
+        where: { id: input.id, creatorId: ctx.session.user.id },
+        select: defaultPageSelect,
+      })
+    }),
+})
