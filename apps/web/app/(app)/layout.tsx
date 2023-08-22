05c5838a58f78925d2b9afafc2067d9adb35be34
dangminhngo <dangminhngo.dev@gmail.com>
2023-08-22 22:02:34 +0700

    feat(web): add sidebar to app layout

--- apps/web/app/(app)/layout.tsx
+++ apps/web/app/(app)/layout.tsx
@@ -3,12 +3,14 @@ import * as React from "react"
 import { Toaster } from "@acme/web-ui"
 
 import AuthGuard from "~/components/auth/auth-guard"
+import Sidebar from "~/components/sidebar"
 import Init from "./init"
 
 export default function AuthLayout({ children }: React.PropsWithChildren) {
   return (
     <AuthGuard>
       <div className="flex h-screen max-h-screen min-h-screen items-stretch overflow-hidden">
+        <Sidebar />
         <main className="max-w-[calc(100vw-240px)] flex-1">{children}</main>
       </div>
       <React.Suspense>
