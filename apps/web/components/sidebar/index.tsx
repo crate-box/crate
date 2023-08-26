38b2cfddd82f1c95dbe89c0f88fb4f29b915b8bd
dangminhngo <dangminhngo.dev@gmail.com>
2023-08-27 06:37:10 +0700

    feat(web): allow to resize sidebar's width

--- apps/web/components/sidebar/index.tsx
+++ apps/web/components/sidebar/index.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import * as React from "react"
+import { Resizable } from "re-resizable"
 
 import ProfileButton from "~/components/profile/profile-button"
 import SidebarSkeleton from "../skeletons/sidebar-skeleton"
@@ -8,16 +9,27 @@ import SidebarContent from "./sidebar-content"
 
 export default function Sidebar() {
   return (
-    <nav className="relative min-w-[240px] bg-slate-950">
+    <Resizable
+      className="relative min-w-[240px] bg-slate-950"
+      minWidth={240}
+      maxWidth={400}
+      enable={{
+        top: false,
+        right: true,
+        bottom: false,
+        left: false,
+        topRight: false,
+        bottomRight: false,
+        bottomLeft: false,
+        topLeft: false,
+      }}
+    >
       <div className="flex h-full w-full flex-col items-stretch gap-2">
         <React.Suspense fallback={<SidebarSkeleton />}>
           <ProfileButton />
           <SidebarContent />
         </React.Suspense>
       </div>
-      <div className="absolute right-0 top-0 h-full w-[2px] bg-destructive">
-        &nbsp;
-      </div>
-    </nav>
+    </Resizable>
   )
 }
