fa4d307cebea1409ebf18b8f54059a5609998d42
dangminhngo <dangminhngo.dev@gmail.com>
2023-08-27 13:18:30 +0700

    style(web): make sign-in page responsive

--- apps/web/app/(auth)/sign-in/page.tsx
+++ apps/web/app/(auth)/sign-in/page.tsx
@@ -9,11 +9,11 @@ export default function SignIn({
   searchParams: { error: string }
 }) {
   return (
-    <div className="grid h-screen place-items-center bg-slate-950">
-      <div className="flex min-w-[360px] max-w-[360px] flex-col items-center rounded-lg bg-slate-900 p-8">
+    <div className="grid h-screen place-items-center bg-slate-950 px-4">
+      <div className="xs:w-[360px] flex w-full flex-col items-center rounded-lg bg-slate-900 p-6">
         <Logo className="h-12 w-12 text-primary" />
-        <h1 className="mt-8 text-3xl font-bold">Welcome back!</h1>
-        <p className="text-lg text-slate-300">
+        <h1 className="mt-8 text-center text-3xl font-bold">Welcome back!</h1>
+        <p className="text-center text-lg text-slate-300">
           We&apos;re so excited to see you again!
         </p>
         {searchParams.error && (
