7e12c8c9e4d30a5c3af09d720b8fb6014baaa89b
dangminhngo <dangminhngo.dev@gmail.com>
2023-08-27 23:14:18 +0700

    chore(web): format tailwind classes

--- apps/web/app/(auth)/sign-in/page.tsx
+++ apps/web/app/(auth)/sign-in/page.tsx
@@ -10,7 +10,7 @@ export default function SignIn({
 }) {
   return (
     <div className="grid h-screen place-items-center bg-slate-950 px-4">
-      <div className="xs:w-[360px] flex w-full flex-col items-center rounded-lg bg-slate-900 p-6">
+      <div className="flex w-full flex-col items-center rounded-lg bg-slate-900 p-6 xs:w-[360px]">
         <Logo className="h-12 w-12 text-primary" />
         <h1 className="mt-8 text-center text-3xl font-bold">Welcome back!</h1>
         <p className="text-center text-lg text-slate-300">
