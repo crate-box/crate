53fc2845e452a0eee114c07cddec370d6b1542bb
dangminhngo <dangminhngo.dev@gmail.com>
2023-09-05 18:22:22 +0700

    feat(icons): add PlayArrowIcon

--- packages/web-ui/icons.tsx
+++ packages/web-ui/icons.tsx
@@ -486,6 +486,21 @@ function PersonAddIcon(props: SVGProps) {
   )
 }
 
+function PlayArrowIcon(props: SVGProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="24px"
+      viewBox="0 0 24 24"
+      width="24px"
+      fill="currentColor"
+      {...props}
+    >
+      <path d="M8 6.82v10.36c0 .79.87 1.27 1.54.84l8.14-5.18c.62-.39.62-1.29 0-1.69L9.54 5.98C8.87 5.55 8 6.03 8 6.82z" />
+    </svg>
+  )
+}
+
 function RemoveIcon(props: SVGProps) {
   return (
     <svg
@@ -662,6 +677,7 @@ export {
   PaletteIcon,
   PersonIcon,
   PersonAddIcon,
+  PlayArrowIcon,
   RemoveIcon,
   RocketIcon,
   SearchIcon,
